1b40c1b5d82786511020bd12c5ec0b81f8525aef
brianmanderson <markba122@gmail.com>
2022-01-30 18:35:11 -0800

    have a combo box, delete and archive buttons

--- SterillizationTracking/Windows/CategoryWindow.xaml.cs
+++ SterillizationTracking/Windows/CategoryWindow.xaml.cs
@@ -19,15 +19,60 @@ namespace SterillizationTracking.Windows
     /// <summary>
     /// Interaction logic for CategoryWindow.xaml
     /// </summary>
-    public partial class CategoryWindow : Window
+    public partial class CategoryWindow : Window, INotifyPropertyChanged
     {
         private string applicator_directory;
         private string category_name;
         private string total_uses;
+        public event PropertyChangedEventHandler PropertyChanged;
+        private List<string> _category_names = new List<string> {};
+        public List<string> Category_Names
+        {
+            get
+            {
+                return _category_names;
+            }
+            set
+            {
+                _category_names = value;
+                OnPropertyChanged("Category_Names");
+            }
+        }
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
         public CategoryWindow(string applicator_directory)
         {
             InitializeComponent();
             this.applicator_directory = applicator_directory;
+            Rebuild();
+        }
+        private void Rebuild()
+        {
+            string[] applicator_list = Directory.GetDirectories(applicator_directory);
+            Category_Names = new List<string> { };
+            foreach (string applicator in applicator_list)
+            {
+                string[] temp_list = applicator.Split('\\');
+                string applicator_name = temp_list[temp_list.Length - 1];
+                if (applicator_name != "Archived")
+                {
+                    Category_Names.Add(applicator_name);
+                }
+            }
+            bind();
+        }
+        private void bind()
+        {
+            Binding category_binding = new Binding("Category_Names");
+            category_binding.Source = this;
+            CategoriesComboBox.SetBinding(ComboBox.ItemsSourceProperty, category_binding);
         }
         private void CheckButton()
         {
@@ -70,6 +115,43 @@ namespace SterillizationTracking.Windows
             }
             UsesText.Text = "";
             CategoryText.Text = "";
+        }
+
+        private void CheckBox_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        private void CheckBoxChanged(object sender, RoutedEventArgs e)
+        {
+            bool delete_checked = DeleteCheckBox.IsChecked ?? false;
+            bool archived_check = ArchiveCheckBox.IsChecked ?? false;
+            if (delete_checked)
+            {
+                ArchiveCheckBox.IsEnabled = false;
+                ProcessButton.Content = "Delete";
+                ProcessButton.IsEnabled = true;
+            }
+            else if (archived_check)
+            {
+                DeleteCheckBox.IsEnabled = false;
+                ProcessButton.Content = "Archive";
+                ProcessButton.IsEnabled = true;
+            }
+            else
+            {
+                ProcessButton.IsEnabled = false;
+                ArchiveCheckBox.IsEnabled = true;
+                DeleteCheckBox.IsEnabled = true;
+            }
+        }
+
+        private void CategoriesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CategoriesComboBox.SelectedItem != null)
+            {
+                ArchiveCheckBox.IsEnabled = true;
+                DeleteCheckBox.IsEnabled = true;
+            }
         }
     }
 }
